c718ce0beadf38e17ae45d677d6e84363993cf72
Jo Boermans <boermansjo@gmail.com>
2023-04-13 14:29:59 +0200

    ➕ Add Player model and service

---
+++ MauiApp/Services/PlayerService.cs
@@ -0,0 +1,26 @@
+﻿using MauiApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MauiApp.Services
+{
+    internal class PlayerService
+    {
+        private readonly string _playersFilePath;
+
+        public PlayerService(string playersFilePath)
+        {
+            _playersFilePath = playersFilePath;
+        }
+
+        public List<Player> GetPlayers()
+        {
+            string json = File.ReadAllText(_playersFilePath);
+            return JsonSerializer.Deserialize<List<Player>>(json);
+        }
+    }
+}
