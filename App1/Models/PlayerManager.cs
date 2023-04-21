d6c483a4648a4393bd821e3580d0cb8a43a04b68
Jo Boermans <boermansjo@gmail.com>
2023-04-21 14:23:14 +0200

    Add UWP Project

---
+++ App1/Models/PlayerManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace App1.Models
+{
+    internal class PlayerManager
+    {
+        private const string FILE_NAME = "players.json";
+
+        public static List<PlayerModel> LoadPlayers()
+        {
+            var list = new List<PlayerModel>();
+
+            try
+            {
+                using (var stream = File.OpenRead(FILE_NAME))
+                    using (var reader = new StreamReader(stream))
+                {
+                    var json = reader.ReadToEnd();
+                    list = JsonSerializer.Deserialize<List<PlayerModel>>(json);
+                }
+            }
+            catch { }
+            return list;
+        }
+
+        public static void SavePlayers(List<PlayerModel> players)
+        {
+            try
+            {
+                var options = new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                };
+
+                var json = JsonSerializer.Serialize(players, options);
+                File.WriteAllText(FILE_NAME, json);
+            }
+            catch { }
+        }
+    }
+}
---
+++ App1/Models/PlayerModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.Models
+{
+    internal class PlayerModel
+    {
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string Ranking { get; set; }
+
+    }
+}
